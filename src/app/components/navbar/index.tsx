b00454a90fdcdced1ce8b13126dd0ebf223e4354
DiYaZeN <minolamarakoon@gmail.com>
2023-06-02 15:27:08 +0530

    2nd

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -2,28 +2,31 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Logo } from "../logo";
+import { NavItems } from "./navItems";
 
 const NavBarContainer = styled.div`
-    min-height: 68px;
-    ${tw` 
-        w-full
-        max-w-2xl
-        flex
-        flex-row
-        items-center
-        lg:pl-12
-        lg:pr-12
-        justify-between
-    `}
+  min-height: 68px;
+  ${tw`
+    w-full
+    max-w-screen-2xl
+    flex
+    flex-row
+    items-center
+    lg:pl-12
+    lg:pr-12
+    justify-between
+  `};
 `;
 
 const  LogoContainer = styled.div``;    
  
-
 export function NavBar() {
-    return <NavBarContainer>
+    return (
+      <NavBarContainer>
         <LogoContainer>
-            <Logo />
+          <Logo />
         </LogoContainer>
-    </NavBarContainer> 
-}
+        <NavItems></NavItems>
+      </NavBarContainer>
+    );
+  }
--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -2,32 +2,58 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 
-const ListContainer = styled.ul`
-    ${tw`
-        flex
-        list-none
-    `}  
-`; 
 
-const NavigationItem = styled.li` 
-    ${tw`
-        text-xs
-        md:text-base  
-        text-black
-        font-medium
-        mr-1
-        md:mr-5
-        cursor-pointer
-        transition
-        duration-300
-        ease-in-out
-        hover:text-gray-700
-    `}
+const ListContainer = styled.ul`
+  ${tw`
+    flex
+    list-none
+  `};
 `;
 
 
+const NavigationItem = styled.li`
+  ${tw`
+    text-sm
+    md:text-base
+    text-black
+    font-medium
+    mr-1
+    md:mr-5
+    cursor-pointer
+    transition
+    duration-300
+    ease-in-out
+    
+  `};
 
- 
+  a {
+    ${tw`
+      no-underline
+      text-black
+    `};
+
+    &:hover {
+      ${tw`
+        text-gray-500
+      `};
+    }
+  }
+`;
+
  export function NavItems () {
-    return <div>NavItems</div>
+    return (
+        <ListContainer>
+          <NavigationItem >
+            <a href="#1">Home</a>
+          </NavigationItem>
+          <NavigationItem >
+            <a href="#2">Cars</a>
+          </NavigationItem>
+          <NavigationItem >
+            <a href="#3">Services</a>
+          </NavigationItem>
+          <NavigationItem >
+            <a href="#4">Contact Us</a>
+          </NavigationItem>
+        </ListContainer>);
 }
